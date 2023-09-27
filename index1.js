4e2c4e9fd4f75e60d6a431cc66044fee8a5859fb
Andrelegrand <andremabutwa1@gmail.com>
2023-09-27 11:12:16 +0200

    ajout fonction soustraction

--- index1.js
+++ index1.js
@@ -1,8 +1,11 @@
-
+function minute(a,b) {
+    let reusltat =a-b; 
+    return reusltat;
+}
 
 function calcul(a, b, operator) {
     if (operator == '+') return addition(a, b);
     if (operator == '-') return minute(a, b);
 
 }
-console.log(calcul());
+console.log(calcul(3,10,'-'));
